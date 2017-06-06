17ee1af0757cdbd4253ac1f8240da6578c92205a
valouvaliavlo <cosseval@hotmail.fr>
2017-06-06 14:10:51 +0200

    serial
    fix serial receive

--- SIMU_MACHINE_ETATS/SIMU_MACHINE_ETATS.ino
+++ SIMU_MACHINE_ETATS/SIMU_MACHINE_ETATS.ino
@@ -1,58 +1,67 @@
 String data;
 String Etat;
 void setup() {
+Serial1.begin(9600);
 Serial.begin(9600);
-data="DIAPO";
+data="";
 }
 
 void loop() {
-  if(data!="")Etat=(String)data;
-  data="";
+  if(data!="")
+  {
+    Etat=(String)data;
+      data="";
+    Serial.println(Etat);
+  }
+  delay(100);
   if(Etat.equals(String("DIAPO")))
   {
-    Serial.print("E1\n");
+    Serial1.print("E1\n");
     delay(3000);
-    Serial.print("E2\n");
+    Serial1.print("E2\n");
     delay(3000);
-    Serial.print("E20\n");
+    Serial1.print("E20\n");
     delay(3000);
-    Serial.print("E24\n");
+    Serial1.print("E24\n");
     delay(200);
-    Serial.print("C1C2\n");
+    Serial1.print("C1C2\n");
     delay(3000);
-    Serial.print("E25\n");
+    Serial1.print("E25\n");
     delay(200);
-    Serial.print("A5H\t5L\t12\t3,3\t24\n");
+    Serial1.print("A5H\t5L\t12\t3,3\t24\n");
     delay(3000);
   }
   else if(Etat.equals(String("BTN_AV")))
   {
-    Serial.print("E3\n");
     delay(200);
-    for(int i =0; i<50; i++)
+    Serial1.print("E3\n");
+    delay(200);
+    for(int i =0; i<12; i++)
     {
-      Serial.print("I"+String(i)+"\n");
+      Serial1.print("I"+String(i)+"\n");
       delay(500);
     }
+    Etat="";
   }
     else if (Etat.equals(String("BTN_AR")))
     {
-      Serial.print("E12\n");
       delay(200);
-      for(int i =0; i<50; i++)
+      Serial1.print("E12\n");
+      delay(200);
+      for(int i =0; i<12; i++)
       {
-        Serial.print("I"+String(i)+"\n");
+        Serial1.print("I"+String(i)+"\n");
         delay(500);
       }
+          Etat="";
     }
   }
   
-}
-void serialEvent()
+void serialEvent1()
 {
-   while (Serial.available())                  // Boucle tant que la reception est active
+   while (Serial1.available())                  // Boucle tant que la reception est active
   {
-    char in = (char)Serial.read(); //
+    char in = (char)Serial1.read(); //
     if(in =='\n') break;
     else data += in;               //On concatene un string avec les donnés reçus
   }
